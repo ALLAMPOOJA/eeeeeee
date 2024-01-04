87ba1a82f694f707d06503ff27bbf8695302972a
POOJA <poojaaallam@gmail.com>
2024-01-04 15:58:05 +0530

    second commit

---
+++ src/HI/HRRRRR.java
@@ -0,0 +1,12 @@
+package HI;
+
+public class HRRRRR {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("HI POOOOOOOOJJJJJJJJJJAAAAAAA");
+
+	}
+
+}
